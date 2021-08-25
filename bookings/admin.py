8647a50f3323bb90aa1915efd76ad13a07e66570
OliHickie <oli.h@hotmail.co.uk>
2021-08-25 11:07:38 +0000

    Add function to save booking form to admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import NewBooking
 
-# Register your models here.
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'wedding_date',
+                    'venue_name', 'email', 'contact_number',)
+
+    ordering = ('wedding_date',)
+
+
+admin.site.register(NewBooking, BookingAdmin)
--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BookingForm
 
 from bands.models import Band
@@ -6,14 +6,40 @@ from bands.models import Band
 
 def new_booking(request, band_id):
     """
-    A view to display booking form
+    A view to display booking form and process booking
     """
     band = get_object_or_404(Band, pk=band_id)
     form = BookingForm()
 
-    context = {
-        'band': band,
-        'form': form,
+    if request.method == 'POST':
+        # booking = request.session.get('booking', {})
+
+        booking_form = {
+            'client_name': request.POST['client_name'],
+            'email': request.POST['email'],
+            'contact_number': request.POST['contact_number'],
+            'venue_name': request.POST['venue_name'],
+            'venue_address1': request.POST['venue_address1'],
+            'venue_address2': request.POST['venue_address2'],
+            'county': request.POST['county'],
+            'postcode': request.POST['postcode'],
+            'wedding_date': request.POST['wedding_date'],
+            'start_time': request.POST['start_time'],
+            'emergency_contact': request.POST['emergency_contact'],
+            'emergency_phone': request.POST['emergency_phone'],
+            'additional_information': request.POST['additional_information'],
         }
+
+        form = BookingForm(booking_form)
+        if form.is_valid():
+            form.save()
+
+        return redirect('all_bands')
+
+    context = {
+            'band': band,
+            'form': form,
+            # 'booking': booking,
+            }
 
     return render(request, 'bookings/new_booking.html', context)
