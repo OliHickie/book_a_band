425848bde3fe4df5f238d4e3ce3e31107b8710cf
OliHickie <oli.h@hotmail.co.uk>
2021-08-26 11:54:33 +0000

    Create template to read blog and link to cards

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
@@ -14,3 +14,17 @@ def all_blogs(request):
     }
 
     return render(request, 'blogs/blogs.html', context)
+
+
+def read_blog(request, blog_id):
+    """
+    A view to display blog content
+    """
+
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blogs/read-blog.html', context)
