429bcce9488d8d3d391017f870a5c10967f01cd2
OliHickie <oli.h@hotmail.co.uk>
2021-08-26 10:40:01 +0000

    Add blog cards to blog page

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'updated_at',)
+
+    ordering = ('updated_at',)
+
+
+admin.site.register(Blog, BlogAdmin)
