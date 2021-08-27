82d324fd31c3e67f5215cd0738e5e1c92e04c59d
OliHickie <oli.h@hotmail.co.uk>
2021-08-27 15:58:26 +0000

    Create function to change main image on band profile

--- bands/static/bands/js/band_profile.js
+++ bands/static/bands/js/band_profile.js
@@ -1,5 +1,21 @@
+function imageDisplay() {
+    $('.profile-sm-img').click(function() {
+        let clickedImage = $(this).attr('src')
+        let mainImage = $('.profile-main-img').attr('src')
 
+        $(this).fadeOut(500, function(){
+            $(this).attr('src', mainImage)
+            $(this).fadeIn()
+        })
+        $('.profile-main-img').fadeOut(500, function(){
+            $('.profile-main-img').attr('src', clickedImage)
+            $('.profile-main-img').fadeIn()
+        })
 
-$(document).ready(function () {
+    })
+}
 
-})
+$(document).ready(function() {
+    imageDisplay()
+});
+    
