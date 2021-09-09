6ff562399044343b5897ad1901a8dbc471e523dd
OliHickie <oli.h@hotmail.co.uk>
2021-09-09 14:34:35 +0000

    Temp remove cache control from settings

--- bookaband/settings.py
+++ bookaband/settings.py
@@ -186,10 +186,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
     # Cache Control
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000'
-    }
+    # AWS_S3_OBJECT_PARAMETERS = {
+    #     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    #     'CacheControl': 'max-age=94608000'
+    # }
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'bookaband'
