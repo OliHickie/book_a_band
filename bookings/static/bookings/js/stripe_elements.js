faa37e6c01c99f20de068bf765b9ccae80ecfc63
OliHickie <oli.h@hotmail.co.uk>
2021-09-13 17:13:07 +0000

    Complete payment process with stripe

--- bookings/static/bookings/js/stripe_elements.js
+++ bookings/static/bookings/js/stripe_elements.js
@@ -1,8 +1,13 @@
-// Get stripe public key and client secret from template
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
-
-// set up stripe
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
@@ -20,21 +25,62 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', {style: style});
+var card = elements.create('card', {
+    style: style
+});
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
-card.addEventListener('change', function(event){
+card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
         var html = `
-        <span class="icon" role="alert">
-            <i class="fas fa-times"></i>
-        </span>
-        <span><small>${event.error.message}</small></span>
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
         `;
         $(errorDiv).html(html);
     } else {
         errorDiv.textContent = '';
     }
-})
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    card.update({
+        'disabled': true
+    });
+
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+            billing_details: {
+                name: 'Jenny Rosen',
+            },
+        },
+    }).then(function (result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+            card.update({
+                'disabled': false
+            });
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
-
+import datetime
 import stripe
 
 from .forms import BookingForm
@@ -19,6 +19,9 @@ def new_booking(request, band_id):
 
     if request.method == 'POST':
 
+        date_input = request.POST['wedding_date']
+        wedding_date = datetime.datetime.strptime(date_input, "%d-%m-%Y").strftime("%Y-%m-%d")
+
         booking_form = {
             'client_name': request.POST['client_name'],
             'contact_number': request.POST['contact_number'],
@@ -27,7 +30,7 @@ def new_booking(request, band_id):
             'venue_address2': request.POST['venue_address2'],
             'county': request.POST['county'],
             'postcode': request.POST['postcode'],
-            'wedding_date': request.POST['wedding_date'],
+            'wedding_date': wedding_date,
             'start_time': request.POST['start_time'],
             'emergency_contact': request.POST['emergency_contact'],
             'emergency_phone': request.POST['emergency_phone'],
@@ -60,6 +63,8 @@ def my_bookings(request):
     user = request.user.email
     bookings = NewBooking.objects.filter(email=user).order_by('paid')
 
+    print("Bookings", bookings)
+
     confirmed_bookings = bookings.filter(paid=True)
     unconfirmed_bookings = bookings.filter(paid=False)
     total = 0
@@ -86,19 +91,18 @@ def payments(request):
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     balance = request.session['balance']
-    stripe_balance = int(balance)
+    stripe_balance = int(balance * 100)
     stripe.api_key = stripe_secret_key
     intent = stripe.PaymentIntent.create(
         amount=stripe_balance,
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     context = {
         'stripe_public_key': stripe_public_key,
-        'client_secret': stripe_secret_key,
+        'client_secret': intent.client_secret,
         'stripe_balance': stripe_balance,
+        'balance': balance,
     }
 
     return render(request, 'bookings/payments.html', context)
