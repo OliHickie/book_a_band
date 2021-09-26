9b0856a3b4252fed3af0cfa40463af1326e330dc
OliHickie <oli.h@hotmail.co.uk>
2021-09-26 15:35:44 +0000

    Bands - Edit band view and template added

--- bands/urls.py
+++ bands/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:band_id>/', views.band_profile, name='band_profile'),
     path('add_band/', views.add_band, name='add_band'),
     path('add_review/<int:band_id>/', views.add_review, name='add_review'),
+    path('edit_band/<int:band_id>/', views.edit_band, name='edit_band'),
 ]
--- bands/views.py
+++ bands/views.py
@@ -178,3 +178,27 @@ def add_review(request, band_id):
     return render(request, 'bands/add_review.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
+def edit_band(request, band_id):
+    """
+    A view to edit band details
+    """
+    band = get_object_or_404(Band, pk=band_id)
+
+    if request.method == 'POST':
+        form = BandForm(request.POST, request.FILES, instance=band)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The band has been successfully updated')
+
+        return redirect(reverse('band_profile', kwargs={'band_id': band_id}))
+
+    else:
+        form = BandForm(instance=band)
+
+    context = {
+        'form': form,
+        'band': band,
+    }
+    
+    return render(request, 'bands/edit_band.html', context)    
